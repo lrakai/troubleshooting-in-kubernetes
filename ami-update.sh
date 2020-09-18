0b4fd77493d69f08fd684ca5bd2ec2f4ec24d02e
lrakai <lmrakai@gmail.com>
2020-09-18 15:17:37 -0600

    k8s 1.19

---
+++ ami-update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# FROM: Ubuntu 18.04 latest us-west-2 AMI
+
+sudo -i
+apt-get update
+apt-get -y upgrade
+
+apt-get -y install docker.io
+systemctl enable --now docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+apt-get install kubeadm=1.19.2-00 kubectl=1.19.2-00 kubelet=1.19.2-00 kubernetes-cni=0.8.7-00 -y -f
+apt-mark hold kubeadm kubelet kubectl kubernetes-cni
+echo v1.19.2 > /etc/kubernetes_community_ami_version
+apt-get install ec2-instance-connect
+
+history -c
