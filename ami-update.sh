8f2db22fa17af6f5d8a9f608abc1d3eb006ad772
lrakai <lmrakai@gmail.com>
2020-09-18 17:47:22 -0600

    Add cfn tools

--- ami-update.sh
+++ ami-update.sh
@@ -3,6 +3,7 @@
 # FROM: Ubuntu 18.04 latest us-west-2 AMI
 
 sudo -i
+
 apt-get update
 apt-get -y upgrade
 
@@ -14,6 +15,9 @@ apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt-get install kubeadm=1.19.2-00 kubectl=1.19.2-00 kubelet=1.19.2-00 kubernetes-cni=0.8.7-00 -y -f
 apt-mark hold kubeadm kubelet kubectl kubernetes-cni
 echo v1.19.2 > /etc/kubernetes_community_ami_version
-apt-get install ec2-instance-connect
+
+apt-get -y install python-pip
+pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+apt-get -y install ec2-instance-connect 
 
 history -c
